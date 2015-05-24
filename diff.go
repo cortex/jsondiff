b6a5f7b4d5f75f8937c8ab0cc0e0352c7d30bf09
Joakim Lundborg <joakim.lundborg@gmail.com>
2015-05-25 01:51:50 +0200

    Add support for json test suite, initial support for arrays

--- diff.go
+++ diff.go
@@ -3,74 +3,118 @@ package jsondiff
 import "encoding/json"
 import "github.com/deckarep/golang-set"
 import "errors"
-
-type Dict map[string]interface{}
+import "reflect"
+import "fmt"
+import "path"
 
 type PatchOp struct {
-	Op    string  `json:"op"`
-	Path  string  `json:"path"`
-	Value *string `json:"value,omitempty"`
+	Op    string      `json:"op"`
+	Path  string      `json:"path"`
+	Value interface{} `json:"value,omitempty"`
 }
 
-func Diff(inb []byte, outb []byte) (ops []PatchOp, err error) {
+func DiffBytes(inb []byte, outb []byte) (ops []PatchOp, err error) {
 
-	var in Dict
+	var in json.RawMessage
 	err = json.Unmarshal(inb, &in)
 	if err != nil {
 		return nil, err
 	}
 
-	var out Dict
+	var out json.RawMessage
 	err = json.Unmarshal(outb, &out)
 	if err != nil {
 		return nil, err
 	}
+	return DiffRaw(in, out)
 
-	extra, missing := KeyDiff(in, out)
+}
 
-	if extra.Cardinality() == 0 && missing.Cardinality() == 0 {
-		return nil, nil
+func DiffRaw(doc1 json.RawMessage, doc2 json.RawMessage) (ops []PatchOp, err error) {
+	var i1 interface{}
+	var i2 interface{}
+	err = json.Unmarshal(doc1, &i1)
+	if err != nil {
+		return
 	}
+	err = json.Unmarshal(doc2, &i2)
+	if err != nil {
+		return
+	}
+	return Diff(i1, i2, "/")
+}
 
-	if extra.Cardinality() > 0 && missing.Cardinality() == 0 {
-		for _, e := range extra.ToSlice() {
-			ops = append(ops, PatchOp{"remove", "/" + e.(string), nil})
-		}
+func Diff(doc1 interface{}, doc2 interface{}, root string) (ops []PatchOp, err error) {
+	// If the docs have different types, just replace
+	if reflect.TypeOf(doc1) != reflect.TypeOf(doc2) {
+		ops = append(ops, PatchOp{"replace", "/", doc1})
 		return ops, nil
 	}
 
-	if extra.Cardinality() == 0 && missing.Cardinality() > 0 {
-		for _, e := range missing.ToSlice() {
-			value := out[e.(string)]
-			v, _ := json.Marshal(value)
-			vs := string(v[1 : len(v)-1]) // FIXME: only works for strings
-			ops = append(ops, PatchOp{"add", "/" + e.(string), &vs})
+	// Type-specific handling
+	switch t1 := doc1.(type) {
+	case int:
+		if t1 != doc2 {
+			ops = append(ops, PatchOp{"replace", root, doc2})
 		}
-		return ops, nil
+	case string:
+		if t1 != doc2 {
+			ops = append(ops, PatchOp{"replace", root, doc2})
+		}
+	case bool:
+		if t1 != doc2 {
+			ops = append(ops, PatchOp{"replace", root, doc2})
+		}
+	case map[string]interface{}:
+		t2 := doc2.(map[string]interface{})
+		extra, missing := KeyDiff(&t1, &t2)
+		if extra.Cardinality() == 0 && missing.Cardinality() == 0 {
+			return nil, nil
+		}
+
+		if extra.Cardinality() > 0 && missing.Cardinality() == 0 {
+			for _, e := range extra.ToSlice() {
+				ops = append(ops, PatchOp{"remove", path.Join(root, e.(string)), nil})
+			}
+			return ops, nil
+		}
+
+		if extra.Cardinality() == 0 && missing.Cardinality() > 0 {
+			for _, e := range missing.ToSlice() {
+				value := (t2)[e.(string)]
+				ops = append(ops, PatchOp{"add", path.Join(root + e.(string)), value})
+			}
+			return ops, nil
+		}
+
+	case []interface{}:
+		t2 := doc2.([]interface{})
+		if len(t1) == len(t2) {
+			for i := range t1 {
+				Diff(t1[i], t2[i], path.Join(root, string(i)))
+			}
+		}
+
+	default:
+		return nil, errors.New(fmt.Sprintf("Not implemented: %v", reflect.TypeOf(t1)))
 	}
-
-	// TODO:
-	// if len extra > 0 and len (missing) > 0
-	// add and remove
-	// future: rename?
-
-	return nil, errors.New("Not implemented")
+	return
 }
 
 // KeyDiff compares the keys in two string-maps
 // extra is the keys in b not in a
 // missing is the keys in a not in b
 
-func KeyDiff(a, b Dict) (extra, missing mapset.Set) {
+func KeyDiff(a, b *map[string]interface{}) (extra, missing mapset.Set) {
 
 	aKeys := mapset.NewSet()
 	bKeys := mapset.NewSet()
 
-	for k := range a {
+	for k := range *a {
 		aKeys.Add(k)
 	}
 
-	for k := range b {
+	for k := range *b {
 		bKeys.Add(k)
 	}
 	return aKeys.Difference(bKeys), bKeys.Difference(aKeys)
--- jsondiff_test.go
+++ jsondiff_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/evanphx/json-patch"
+	"os"
+	"reflect"
 	"testing"
 )
 
@@ -41,9 +43,68 @@ func TestSimpleRemove(t *testing.T) {
    ]`)
 	verifyPatch(t, a, b, expected)
 }
+
+type TestCase struct {
+	Doc           json.RawMessage `json:"doc"`      // The JSON document to test against
+	Patch         []PatchOp       `json:"patch"`    // The patch(es) to apply
+	Expected      json.RawMessage `json:"expected"` // The expected resulting document, OR
+	ExpectedError string          `json:"error"`    // A string describing an expected error
+	Comment       string          `json:"comment"`  // A string describing the test
+	Disabled      bool            `json:"disabled"` // True if the test should be skipped
+}
+
+func TestSuite(t *testing.T) {
+	var ok, failed, errors, skipped int
+
+	testf, err := os.Open("tests/spec_tests.json")
+	if err != nil {
+		t.Fatalf("Failed to open test suite")
+	}
+	dec := json.NewDecoder(testf)
+	var testCases []TestCase
+	err = dec.Decode(&testCases)
+	if err != nil {
+		t.Fatalf("Failed to parse test cases: %v", err)
+	}
+	for _, test := range testCases {
+		if test.ExpectedError != "" {
+			t.Log("== SKIP: ", test.Comment)
+			skipped++
+			continue
+		}
+		t.Log("== RUN: ", test.Comment)
+		t.Log("In: ", string(test.Doc))
+		t.Log("Out: ", string(test.Expected))
+		t.Log("Expected patch: ", test.Patch)
+
+		patch, err := DiffRaw(test.Doc, test.Expected)
+		if err != nil {
+			t.Log("Error: ", err)
+			t.Error(test.Comment, err)
+			t.Log("-- ERROR\n")
+			errors++
+			continue
+		}
+		t.Log("Generated patch", test.Patch)
+
+		if !(reflect.DeepEqual(patch, test.Patch) ||
+			len(patch) == len(test.Patch) && len(patch) == 0) {
+			t.Logf("Failed: not equal: %v %v", patch, test.Patch)
+			t.Fail()
+			t.Log("-- FAIL\n")
+			failed++
+			continue
+		}
+		fmt.Println(test.Comment, patch)
+		t.Log("-- OK\n")
+		ok++
+	}
+	t.Logf("OK: %v Failed: %v Error: %v Skipped: %v", ok, failed, errors, skipped)
+}
+
 func verifyPatch(t *testing.T, in []byte, out []byte, expected []byte) {
 
-	patchObj, err := Diff(in, out)
+	patchObj, err := DiffBytes(in, out)
 	if err != nil {
 		t.Error(err)
 	}
