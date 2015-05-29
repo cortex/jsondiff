04726a48a2330354b87e134f8ab48fd58a6c2b0b
Joakim Lundborg <lundborg@wrapp.com>
2015-05-29 17:39:36 +0200

    Working test generator

--- testgen/main.go
+++ testgen/main.go
@@ -1,10 +1,15 @@
 package main
 
-import "encoding/json"
+import (
+	"bytes"
+	"encoding/json"
+	"strconv"
+	"strings"
+)
 import "os"
 import "log"
 import "text/template"
-import "strconv"
+import "regexp"
 import "flag"
 import "fmt"
 
@@ -18,22 +23,54 @@ type TestCase struct {
 	Name          string
 }
 
+const headerTemplate = `package {{.Package}}
+
+import "testing"
+
+`
+
 const testTemplate = `
-//{{.Comment}}
 func Test{{.Name}} (t* testing.T){
-    doc := ` + "`" + `{{printf "%s" .Doc }}` + "`" + `
-    patch := ` + "`" + `{{printf "%s" .Patch}}` + "`" + `
-{{if .ExpectedError}}
-    expected := ` + "`" + `{{printf "%s" .Expected}}` + "`" + `
-    verifyPatch(doc, patch, expected)
+	//{{.Comment}}
+	doc := []byte(` + "`" + `{{printf "%s" .Doc }}` + "`)" + `
+	patch := []byte(` + "`" + `{{printf "%s" .Patch}}` + "`)" + `
+{{if not .ExpectedError}}	expected := ` + "[]byte(`" + `{{printf "%s" .Expected}}` + "`)" + `
+	verifyPatch(t, doc, patch, expected){{else}}expectedError := "{{.ExpectedError}}"
+	verifyPatchError(t, doc, patch, expectedError)
+{{end}}
 }
 `
+
+var camelingRegex = regexp.MustCompile("[0-9A-Za-z]+")
+
+func CamelCase(src string) string {
+	byteSrc := []byte(src)
+	chunks := camelingRegex.FindAll(byteSrc, -1)
+	for idx, val := range chunks {
+		if idx > 0 {
+			chunks[idx] = bytes.Title(val)
+		}
+	}
+	return string(bytes.Join(chunks, nil))
+}
+
+func testName(in string) string {
+	re := regexp.MustCompile("[^0-9A-Za-z ]")
+	return strings.Title(CamelCase(re.ReplaceAllString(in, "")))
+}
 
 func main() {
 	outfile := flag.String("o", "", "output filename")
 	flag.Parse()
+
+	if *outfile == "" {
+		fmt.Println("Outfile is required")
+		os.Exit(1)
+	}
+
 	name := flag.Arg(0)
-	fmt.Println(*outfile)
+	fmt.Println("outfile:", *outfile)
+
 	outf, err := os.Create(*outfile)
 	if err != nil {
 		panic(err)
@@ -43,19 +80,30 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	htmpl, err := template.New("TestCase").Parse(headerTemplate)
+	if err != nil {
+		panic(err)
+	}
+
 	testf, err := os.Open(name)
 	if err != nil {
 		log.Fatalf("Failed to open test suite")
 	}
+
 	dec := json.NewDecoder(testf)
 	var testCases []TestCase
 	err = dec.Decode(&testCases)
+
 	if err != nil {
 		log.Fatalf("Failed to parse test cases: %v", err)
 	}
+	htmpl.Execute(outf, struct{ Package string }{os.Getenv("GOPACKAGE")})
 	for i, test := range testCases {
-		test.Name = strconv.Itoa(i)
-		fmt.Println(test.Name)
+		test.Name = testName(test.Comment)
+		if test.Name == "" {
+			test.Name = strconv.Itoa(i)
+		}
 		err = tmpl.Execute(outf, test)
 	}
 }
